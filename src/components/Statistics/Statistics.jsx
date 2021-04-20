0c7ac60c1681e2738e70115abc438f7658404950
RinaOcean <antyfifa@gmail.com>
2021-04-21 01:38:14 +0300

    добавляет progress bar

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,15 +4,20 @@ import PropTypes from 'prop-types';
 import './Statistics.scss';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
-  const now = positivePercentage;
-
+  const nowGood = positivePercentage;
+  const nowNeutral = Math.ceil((neutral * 100) / total);
+  const nowBad = Math.ceil((bad * 100) / total);
   return (
     <>
-      <ProgressBar variant="success" now={now} label={`${now}%`} />
+      <ProgressBar variant="success" now={nowGood} label={`${nowGood}%`} />
       <p className="Statistics__item">Good: {good}</p>
-      <ProgressBar variant="warning" now={now} label={`${now}%`} />
+      <ProgressBar
+        variant="warning"
+        now={nowNeutral}
+        label={`${nowNeutral}%`}
+      />
       <p className="Statistics__item">Neutral: {neutral}</p>
-      <ProgressBar variant="danger" now={now} label={`${now}%`} />
+      <ProgressBar variant="danger" now={nowBad} label={`${nowBad}%`} />
       <p className="Statistics__item">Bad: {bad}</p>
       <p className="Statistics__item">Total: {total}</p>
       <p className="Statistics__item">
